364dab4a6b6f3617729cf70b0d6aab8b36e9a52a
SmellyCat <smellycat2014@foxmail.com>
2018-04-17 12:09:45 +0800

    添加 报警方法

--- helper_kakou.py
+++ helper_kakou.py
@@ -43,6 +43,38 @@ class Kakou(object):
             self.status = False
             raise
 
+
+    def get_alarm_maxid(self):
+        """获取过车报警最大ID值"""
+        url = 'http://{0}:{1}/alarm/maxid'.format(self.host, self.port)
+        try:
+            r = requests.get(url)
+            if r.status_code == 200:
+                return json.loads(r.text)
+            else:
+                self.status = False
+                raise Exception('url: {url}, status: {code}, {text}'.format(
+                    url=url, code=r.status_code, text=r.text))
+        except Exception as e:
+            self.status = False
+            raise
+
+    def get_alarm_by_id(self, _id):
+        """根据ID范围获取过车报警信息"""
+        url = 'http://{0}:{1}/alarm/{2}'.format(
+            self.host, self.port, _id)
+        try:
+            r = requests.get(url)
+            if r.status_code == 200 or r.status_code == 404:
+                return json.loads(r.text)
+            else:
+                self.status = False
+                raise Exception('url: {url}, status: {code}, {text}'.format(
+                    url=url, code=r.status_code, text=r.text))
+        except Exception as e:
+            self.status = False
+            raise
+
     def get_kkdd_by_id(self, kkdd_id):
         """根据车牌号码获取布控信息"""
         url = 'http://{0}:{1}/kkdd/{2}'.format(
@@ -60,7 +92,7 @@ class Kakou(object):
             raise
 
     def get_traffic_crossing_info_by_id(self, crossing_id):
-        """根据车牌号码获取布控信息"""
+        """根据ID获取卡口地点信息"""
         url = 'http://{0}:{1}/traffic_crossing_info/{2}'.format(
             self.host, self.port, crossing_id)
         try:
@@ -77,7 +109,7 @@ class Kakou(object):
 
 
     def get_control_unit_by_id(self, control_unit_id):
-        """根据车牌号码获取布控信息"""
+        """根据ID获取控制单元信息"""
         url = 'http://{0}:{1}/control_unit/{2}'.format(
             self.host, self.port, control_unit_id)
         try:
