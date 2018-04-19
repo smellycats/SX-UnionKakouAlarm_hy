eaf2f2ed559f58aacc20e02fb481a63936159cbc
SmellyCat <smellycat2014@foxmail.com>
2018-04-19 22:05:52 +0800

    添加 报警信息布控原因

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import time
 import datetime
@@ -14,7 +14,7 @@ from helper_sms import SMS
 from my_yaml import MyYAML
 from my_logger import *
 
-#sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 debug_logging('/var/logs/error.log')
@@ -55,32 +55,50 @@ class BKCPAlarm(object):
         """发送短信"""
         try:
             self.sms.sms_send(content, mobiles)
-            logger.info(content)
-            logger.info(mobiles)
+            logger.info('mobiles={0}, content={1}'.format(mobiles, content))
         except Exception as e:
             logger.error(e)
 
     def get_data(self):
-        maxid = self.kakou.get_maxid()['maxid']
+        maxid = self.kakou.get_alarm_maxid()['maxid']
         if maxid > self.id_flag:
             print('alarm={0}'.format(self.id_flag+1))
-            info = self.kakou.get_vehicle_by_id(self.id_flag+1)
             logger.info('alarm={0}'.format(self.id_flag+1))
+            info = self.kakou.get_alarm_by_id(self.id_flag+1)
             logger.info(info)
-            if info != {}:
-                if arrow.now('PRC') < arrow.get(info['stop_time']) and arrow.now('PRC') < arrow.get(info['pass_time']).to('Asia/Shanghai').replace(hours=-8, minutes=15):
-                    crossing_info = self.kakou.get_traffic_crossing_info_by_id(info['crossing_id'])
-                    logger.info(crossing_info)
-                    if crossing_info == {}:
-                        self.id_flag += 1
-                        return
-                    control_unit = self.kakou.get_control_unit_by_id(crossing_info['control_unit_id'])
-                    logger.info(control_unit)
-                    if control_unit != {}:
-                        content = "[惠阳卡口平台-{0}报警]{1},{2},{3},{4}".format(
-		            control_unit['name'], info['pass_time'], crossing_info['crossing_name'],
-		            self.fx.get(info['direction_index'], '进城'), info['plate_no'])
-                        self.send_sms(content, info['mobiles'])
+            if info == {}:
+                self.id_flag += 1
+                return
+            if arrow.now('PRC') < arrow.get(info['pass_time']).to('Asia/Shanghai').replace(hours=-8, minutes=15):
+                # 卡口地点信息
+                crossing_info = self.kakou.get_traffic_crossing_info_by_id(info['crossing_id'])
+                logger.info(crossing_info)
+                if crossing_info == {}:
+                    self.id_flag += 1
+                    return
+                # 控制单元
+                control_unit = self.kakou.get_control_unit_by_id(crossing_info['control_unit_id'])
+                logger.info(control_unit)
+                if control_unit == {}:
+                    self.id_flag += 1
+                    return
+                # 布控原因
+                if info['disposition_reason'] == '99':
+                    disposition_reason = info['res_str1']
+                else:
+                    sysdict_info = self.get_traffic_sysdict(
+                        {'sysdict_type':1006,'sysdict_code':info['disposition_reason']})
+                    if sysdict_info['total_count'] > 0:
+                        disposition_reason = sysdict_info['items'][0]['sysdict_name']
+                    else:
+                        disposition_reason = ''
+                # 集成发送内容
+                content = "[惠阳卡口平台-{0}报警]{1},{2},{3},{4}{5}".format(
+                    control_unit['name'], info['pass_time'],
+                    crossing_info['crossing_name'],
+                    self.fx.get(info['direction_index'], '进城'),
+                    info['plate_no'], '({0})'.format(disposition_reason))
+                self.send_sms(content, info['mobiles'])
             self.id_flag += 1
 
 
